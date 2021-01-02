dec28881eeeafb4ddd036a080edfa6e6a90e3645
Moritz Schrauth <moritzschrauth@gmail.com>
2021-01-02 21:26:13 +0100

    added piece images

--- Shogi/Shogi/Board.h
+++ Shogi/Shogi/Board.h
@@ -2,6 +2,13 @@
 #define BOARD_H
 
 #include <QWidget>
+#include <array>
+#include <vector>
+#include <memory>
+
+#include "Position.h"
+
+class Piece;
 
 class Board : public QWidget
 {
@@ -15,7 +22,29 @@ public:
 
     explicit Board(QWidget *parent = nullptr);
 
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 
+private:
+    // 2D-Array representing the board
+    using Data = std::array<std::array<std::shared_ptr<Piece>, 9>, 9>;
+    Data _data;
+
+    int fieldWidth = 20;
+
+    // stores how many of each pieces are catured by each player
+    std::array<int, 8> _capturedBlack;
+    std::array<int, 8> _capturedWhite;
+
+    // stores the pieces ON the board belonging to each player
+    std::vector<std::shared_ptr<Piece>> _boardBlack;
+    std::vector<std::shared_ptr<Piece>> _boardWhite;
+
+    Board::Team _activePlayer;
+
+    Position _selectedField;
+    std::vector<Position> _highlightedFields;
 
 };
 
